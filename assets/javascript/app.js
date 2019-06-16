93d020b07ba26e3bfecd1446501edcb73d21f1d1
hausu <kennedymj2@gmail.com>
2019-06-16 15:42:57 -0500

    on child function is updating the page with information from firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,10 +36,10 @@ var firebaseConfig = {
         time = $("#trainTime").val().trim();
         freq = $("#frequency").val().trim();
 
-        console.log(name);
-        console.log(destin);
-        console.log(time);
-        console.log(freq);
+        // console.log(name);
+        // console.log(destin);
+        // console.log(time);
+        // console.log(freq);
 
         // Code for the push
         database.ref().push({
@@ -51,6 +51,25 @@ var firebaseConfig = {
         
         });
 
+    });
+
+// Firebase watcher + initial loader
+// ================================================================================
+
+    database.ref().on("child_added", function(childSnapshot) {
+
+        // Log everything that's coming out of snapshot
+        console.log(childSnapshot.val().train);
+        console.log(childSnapshot.val().destination);
+        console.log(childSnapshot.val().departure);
+        console.log(childSnapshot.val().frequency);
+
+        // full list of items to the table
+        $(".train-table").append("<tr><td>" + childSnapshot.val().train + "</td><td>" + childSnapshot.val().destination + "</td><td>" + childSnapshot.val().departure + "</td><td>" + childSnapshot.val().frequency + "</td></tr>");
+
+        // Handle the errors
+        }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
     });
 
         
